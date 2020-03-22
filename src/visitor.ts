c4e3a23d062adada65d4a81c28a546ec82a140f4
Quinn Blenkinsop <quinn@qw-in.com>
2020-03-21 17:40:24 -0700

    [WIP] trash code but something to work from

--- src/visitor.ts
+++ src/visitor.ts
@@ -17,8 +17,12 @@ import {
   NameNode,
   UnionTypeDefinitionNode,
   DocumentNode,
+  InterfaceTypeDefinitionNode,
+  EnumTypeDefinitionNode,
 } from 'graphql';
-import { snakeCase, pascalCase } from 'change-case';
+import { snakeCase } from 'change-case';
+import { DepGraph } from 'dependency-graph';
+
 import { PydanticPluginRawConfig } from './config';
 
 export const PYTHON_SCALARS = {
@@ -27,6 +31,7 @@ export const PYTHON_SCALARS = {
   Boolean: 'bool',
   Int: 'int',
   Float: 'float',
+  DateTime: 'Any',
 };
 
 // eslint-disable-next-line @typescript-eslint/no-empty-interface
@@ -41,21 +46,26 @@ export class PydanticVisitor extends BaseVisitor<
   private addOptionalImport = false;
   private addListImport = false;
   private addUnionImport = false;
+  private addEnumImport = false;
+
+  private graph = new DepGraph({
+    circular: true,
+  });
 
   constructor(
     rawConfig: PydanticPluginRawConfig,
-    private _schema: GraphQLSchema,
+    private schema: GraphQLSchema,
   ) {
     super(rawConfig, {
       // enumValues: rawConfig.enumValues || {},
       // listType: rawConfig.listType || 'List',
       // package: rawConfig.package || defaultPackageName,
-      scalars: buildScalars(_schema, {}, PYTHON_SCALARS),
+      scalars: buildScalars(schema, {}, PYTHON_SCALARS),
     });
   }
 
   public getImports(): string {
-    const typing = [];
+    const typing = ['Any'];
     const pydantic = ['BaseModel'];
 
     if (this.addOptionalImport) {
@@ -70,6 +80,8 @@ export class PydanticVisitor extends BaseVisitor<
       typing.push(`Union`);
     }
 
+    const enumInput = this.addEnumImport ? 'from enum import Enum' : '';
+
     const typingImport = typing.length
       ? `from typing import ${typing.join(', ')}`
       : '';
@@ -78,7 +90,7 @@ export class PydanticVisitor extends BaseVisitor<
       ? `from pydantic import ${pydantic.join(', ')}`
       : '';
 
-    return [typingImport, pydanticImport].filter(i => i).join('\n');
+    return [enumInput, typingImport, pydanticImport].filter(i => i).join('\n');
   }
 
   protected clearOptional(str: string): string {
@@ -100,53 +112,206 @@ export class PydanticVisitor extends BaseVisitor<
     return convertedName;
   }
 
-  NamedType(node: NamedTypeNode): string {
+  NamedType(node: NamedTypeNode) {
     this.addOptionalImport = true;
 
-    const { name } = node as any;
+    let { name } = node as any;
 
-    return `Optional[${name}]`;
+    if (name === 'any') {
+      name = 'Any';
+    }
+
+    // If not a scalar, create a node
+    if (!this.graph.hasNode(name)) {
+      this.graph.addNode(name);
+    }
+
+    const source = Object.values(this.scalars).includes(name)
+      ? `Optional[${name}]`
+      : `Optional['${name}']`;
+
+    return {
+      id: name,
+      source,
+    };
   }
 
-  ListType(node: ListTypeNode): string {
+  ListType(node: ListTypeNode) {
     this.addListImport = true;
 
-    const { type } = node;
-    return `Optional[List[${type}]]`;
+    const { type } = node as any;
+
+    return {
+      id: type.id,
+      source: `Optional[List[${type.source}]]`,
+    };
   }
 
-  UnionTypeDefinition(node: UnionTypeDefinitionNode): string {
+  NonNullType(node: NonNullTypeNode) {
+    const { type } = node as any;
+
+    return {
+      id: type.id,
+      source: this.clearOptional(type.source),
+    };
+  }
+
+  FieldDefinition(node: FieldDefinitionNode) {
+    const argName = snakeCase(node.name as any);
+
+    const { type } = node as any;
+
+    return {
+      id: type.id,
+      source: indent(`${argName}: ${type.source}`, 2),
+    };
+  }
+
+  EnumTypeDefinition(node: EnumTypeDefinitionNode) {
+    this.addEnumImport = true;
+
+    const { name, values } = node as any;
+
+    const val = values
+      .map((v: any) => indent(`${v.name} = '${v.name}'`, 2))
+      .join('\n');
+    const source = `class ${name}(str, Enum):\n${val}`;
+
+    if (!this.graph.hasNode(name)) {
+      this.graph.addNode(name);
+    }
+
+    return {
+      id: name,
+      source,
+    };
+  }
+
+  UnionTypeDefinition(node: UnionTypeDefinitionNode) {
     this.addUnionImport = true;
 
-    const { name, types } = node;
+    const { name, types } = node as any;
 
-    const unionTypes = (types ?? []).map(t => this.clearOptional(t as any));
+    const unionTypes = (types ?? []).map((t: any) =>
+      this.clearOptional(t.source),
+    );
 
-    return `${name} = Union[${unionTypes.join(', ')}]`;
+    if (!this.graph.hasNode(name)) {
+      this.graph.addNode(name);
+    }
+
+    types.forEach((t: any) => {
+      if (!this.graph.hasNode(t.id)) {
+        this.graph.addNode(t.id);
+      }
+
+      this.graph.addDependency(name, t.id);
+    });
+
+    return {
+      id: name,
+      source: `${name} = Union[${unionTypes.join(', ')}]`,
+    };
   }
 
-  NonNullType(node: NonNullTypeNode): string {
-    const { type } = node;
-
-    return this.clearOptional((type as unknown) as string);
-  }
-
-  FieldDefinition(node: FieldDefinitionNode): string {
-    const argName = snakeCase(node.name as any);
-    return indent(`${argName}: ${node.type}`, 2);
-  }
-
-  ObjectTypeDefinition(node: ObjectTypeDefinitionNode): string {
-    const modelName = pascalCase(node.name as any);
+  InterfaceTypeDefinition(node: InterfaceTypeDefinitionNode) {
+    const modelName = node.name as any;
     const modelDef = `class ${modelName}(BaseModel):`;
-    const modelArguments = (node.fields ?? []).join('\n');
+    const modelArguments = (node.fields ?? [])
+      .map((f: any) => f.source)
+      .join('\n');
 
-    return [modelDef, modelArguments].join('\n');
+    if (!this.graph.hasNode(modelName)) {
+      this.graph.addNode(modelName);
+    }
+
+    (node.fields ?? []).forEach((t: any) => {
+      if (!this.graph.hasNode(t.id)) {
+        this.graph.addNode(t.id);
+      }
+
+      this.graph.addDependency(modelName, t.id);
+    });
+
+    return {
+      id: modelName,
+      source: [modelDef, modelArguments].join('\n'),
+    };
+  }
+
+  ObjectTypeDefinition(node: ObjectTypeDefinitionNode) {
+    const modelName = node.name as any;
+    const interfaces = (node.interfaces as any).map((n: any) =>
+      this.clearOptional(n.source).replace(/'/g, ''),
+    );
+    const is = interfaces.length ? interfaces.join(', ') : 'BaseModel';
+    const modelDef = `class ${modelName}(${is}):`;
+    const modelArguments = (node.fields ?? [])
+      .map((f: any) => f.source)
+      .join('\n');
+
+    if (!this.graph.hasNode(modelName)) {
+      this.graph.addNode(modelName);
+    }
+
+    const deps: any[] = [];
+
+    interfaces.forEach((t: any) => {
+      if (!this.graph.hasNode(t)) {
+        this.graph.addNode(t);
+      }
+
+      deps.push(t);
+      this.graph.addDependency(modelName, t);
+    });
+
+    // (node.fields ?? []).forEach((t: any) => {
+    //   if (!this.graph.hasNode(t.id)) {
+    //     this.graph.addNode(t.id);
+    //   }
+
+    //   deps.push(t.id);
+    //   this.graph.addDependency(modelName, t.id);
+    // });
+
+    return {
+      id: modelName,
+      source: [modelDef, modelArguments].join('\n'),
+      deps,
+    };
   }
 
   Document(node: DocumentNode) {
-    return node.definitions
-      .filter((d: any) => typeof d === 'string')
+    const { definitions } = node as any;
+
+    const nodesInOrder = this.graph
+      .overallOrder()
+      .filter((n: any) => definitions.find((d: any) => d.id === n));
+
+    return nodesInOrder
+      .map((n: any, i) => {
+        const before = this.graph.dependenciesOf(n);
+        const after = this.graph.dependantsOf(n);
+
+        // console.log(n);
+        // console.log(before.filter(b => after.includes(b)));
+
+        const b = this.graph.dependenciesOf(n);
+        const a = nodesInOrder.slice(i);
+        const both = b.filter(bb => a.includes(bb));
+        // console.log(both);
+
+        const def = definitions.find((d: any) => d.id === n);
+
+        const toFix = (def.deps ?? []).filter((d: any) => both.includes(d));
+
+        // toFix.forEach((fix: any) => {
+
+        // });
+
+        return n;
+      })
+      .map((n: any) => definitions.find((d: any) => d.id === n)?.source || '')
       .join('\n\n\n');
   }
 }
